a5b9eb74a445f8b19808ca9cc05ae5e6e4887f41
bluesven869 <bluesven869@gmail.com>
2020-08-24 22:08:34 -0400

    upgrade demonavbar.js

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -15,7 +15,7 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import {
   Collapse,
@@ -37,49 +37,43 @@ import {
 
 import routes from "routes.js";
 
-class Header extends React.Component {
-  state = {
-    isOpen: false,
-    dropdownOpen: false,
-    color: "transparent",
-  };
-  sidebarToggle = React.createRef();
-  toggle = () => {
-    if (this.state.isOpen) {
-      this.setState({
-        color: "transparent",
-      });
+function Header(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropDownOpen] = useState(false);
+  const [color, setColor] = useState("transparent");
+
+  const sidebarToggle = useRef();
+
+  const handleToggle = useCallback(() => {
+    if (isOpen) {
+      setColor("transparent");
     } else {
-      this.setState({
-        color: "white",
-      });
+      setColor("white");
     }
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
-  dropdownToggle = (e) => {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  };
-  getBrand = () => {
-    var name;
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  const handleDropdownToggle = useCallback(() => {
+    setDropDownOpen(!dropdownOpen);
+  }, [dropdownOpen]);
+
+  const getBrand = useCallback(() => {
+    let name;
     routes.map((prop, key) => {
       if (prop.collapse) {
         prop.views.map((prop, key) => {
-          if (prop.path === this.props.location.pathname) {
+          if (prop.path === props.location.pathname) {
             name = prop.name;
           }
           return null;
         });
       } else {
         if (prop.redirect) {
-          if (prop.path === this.props.location.pathname) {
+          if (prop.path === props.location.pathname) {
             name = prop.name;
           }
         } else {
-          if (prop.path === this.props.location.pathname) {
+          if (prop.path === props.location.pathname) {
             name = prop.name;
           }
         }
@@ -87,129 +81,124 @@ class Header extends React.Component {
       return null;
     });
     return name;
-  };
-  openSidebar = () => {
+  }, [props.location.pathname]);
+
+  const openSidebar = useCallback(() => {
     document.documentElement.classList.toggle("nav-open");
-    this.sidebarToggle.current.classList.toggle("toggled");
-  };
+    sidebarToggle.current.classList.toggle("toggled");
+  }, [sidebarToggle]);
+
   // function that adds color white/transparent to the navbar on resize (this is for the collapse)
-  updateColor = () => {
-    if (window.innerWidth < 993 && this.state.isOpen) {
-      this.setState({
-        color: "white",
-      });
+  const updateColor = useCallback(() => {
+    if (window.innerWidth < 993 && isOpen) {
+      setColor("white");
     } else {
-      this.setState({
-        color: "transparent",
-      });
+      setColor("transparent");
     }
-  };
-  componentDidMount() {
-    window.addEventListener("resize", this.updateColor.bind(this));
-  }
-  componentDidUpdate(e) {
-    if (
-      window.innerWidth < 993 &&
-      e.history.location.pathname !== e.location.pathname &&
-      document.documentElement.className.indexOf("nav-open") !== -1
-    ) {
-      document.documentElement.classList.toggle("nav-open");
-      this.sidebarToggle.current.classList.toggle("toggled");
-    }
-  }
-  render() {
-    return (
-      // add or remove classes depending if we are on full-screen-maps page or not
-      <Navbar
-        color={
-          this.props.location.pathname.indexOf("full-screen-maps") !== -1
-            ? "white"
-            : this.state.color
-        }
-        expand="lg"
-        className={
-          this.props.location.pathname.indexOf("full-screen-maps") !== -1
-            ? "navbar-absolute fixed-top"
-            : "navbar-absolute fixed-top " +
-              (this.state.color === "transparent" ? "navbar-transparent " : "")
-        }
-      >
-        <Container fluid>
-          <div className="navbar-wrapper">
-            <div className="navbar-toggle">
-              <button
-                type="button"
-                ref={this.sidebarToggle}
-                className="navbar-toggler"
-                onClick={() => this.openSidebar()}
-              >
-                <span className="navbar-toggler-bar bar1" />
-                <span className="navbar-toggler-bar bar2" />
-                <span className="navbar-toggler-bar bar3" />
-              </button>
-            </div>
-            <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
+  }, [isOpen]);
+
+  useEffect(
+    (e) => {
+      window.addEventListener("resize", updateColor);
+      if (
+        window.innerWidth < 993 &&
+        e.history.location.pathname !== e.location.pathname &&
+        document.documentElement.className.indexOf("nav-open") !== -1
+      ) {
+        document.documentElement.classList.toggle("nav-open");
+        sidebarToggle.current.classList.toggle("toggled");
+      }
+    },
+    [updateColor]
+  );
+
+  return (
+    // add or remove classes depending if we are on full-screen-maps page or not
+    <Navbar
+      color={
+        props.location.pathname.indexOf("full-screen-maps") !== -1
+          ? "white"
+          : color
+      }
+      expand="lg"
+      className={
+        props.location.pathname.indexOf("full-screen-maps") !== -1
+          ? "navbar-absolute fixed-top"
+          : "navbar-absolute fixed-top " +
+            (color === "transparent" ? "navbar-transparent " : "")
+      }
+    >
+      <Container fluid>
+        <div className="navbar-wrapper">
+          <div className="navbar-toggle">
+            <button
+              type="button"
+              ref={sidebarToggle}
+              className="navbar-toggler"
+              onClick={() => openSidebar()}
+            >
+              <span className="navbar-toggler-bar bar1" />
+              <span className="navbar-toggler-bar bar2" />
+              <span className="navbar-toggler-bar bar3" />
+            </button>
           </div>
-          <NavbarToggler onClick={this.toggle}>
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-          </NavbarToggler>
-          <Collapse
-            isOpen={this.state.isOpen}
-            navbar
-            className="justify-content-end"
-          >
-            <form>
-              <InputGroup className="no-border">
-                <Input placeholder="Search..." />
-                <InputGroupAddon addonType="append">
-                  <InputGroupText>
-                    <i className="now-ui-icons ui-1_zoom-bold" />
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-            </form>
-            <Nav navbar>
-              <NavItem>
-                <Link to="#pablo" className="nav-link">
-                  <i className="now-ui-icons media-2_sound-wave" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </Link>
-              </NavItem>
-              <Dropdown
-                nav
-                isOpen={this.state.dropdownOpen}
-                toggle={(e) => this.dropdownToggle(e)}
-              >
-                <DropdownToggle caret nav>
-                  <i className="now-ui-icons location_world" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem tag="a">Action</DropdownItem>
-                  <DropdownItem tag="a">Another Action</DropdownItem>
-                  <DropdownItem tag="a">Something else here</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <NavItem>
-                <Link to="#pablo" className="nav-link">
-                  <i className="now-ui-icons users_single-02" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Account</span>
-                  </p>
-                </Link>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+          <NavbarBrand href="/">{getBrand()}</NavbarBrand>
+        </div>
+        <NavbarToggler onClick={handleToggle}>
+          <span className="navbar-toggler-bar navbar-kebab" />
+          <span className="navbar-toggler-bar navbar-kebab" />
+          <span className="navbar-toggler-bar navbar-kebab" />
+        </NavbarToggler>
+        <Collapse isOpen={isOpen} navbar className="justify-content-end">
+          <form>
+            <InputGroup className="no-border">
+              <Input placeholder="Search..." />
+              <InputGroupAddon addonType="append">
+                <InputGroupText>
+                  <i className="now-ui-icons ui-1_zoom-bold" />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
+          </form>
+          <Nav navbar>
+            <NavItem>
+              <Link to="#pablo" className="nav-link">
+                <i className="now-ui-icons media-2_sound-wave" />
+                <p>
+                  <span className="d-lg-none d-md-block">Stats</span>
+                </p>
+              </Link>
+            </NavItem>
+            <Dropdown
+              nav
+              isOpen={dropdownOpen}
+              toggle={(e) => handleDropdownToggle(e)}
+            >
+              <DropdownToggle caret nav>
+                <i className="now-ui-icons location_world" />
+                <p>
+                  <span className="d-lg-none d-md-block">Some Actions</span>
+                </p>
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem tag="a">Action</DropdownItem>
+                <DropdownItem tag="a">Another Action</DropdownItem>
+                <DropdownItem tag="a">Something else here</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <NavItem>
+              <Link to="#pablo" className="nav-link">
+                <i className="now-ui-icons users_single-02" />
+                <p>
+                  <span className="d-lg-none d-md-block">Account</span>
+                </p>
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
